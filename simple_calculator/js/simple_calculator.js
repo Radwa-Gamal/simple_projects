a23dc2a96f90cad91bbadfe741e5f76ff1bbf904
Bahi Hussein <bahihussein@Bahis-MacBook-Pro.local>
2014-12-15 01:05:18 +0400

    naturalizing

--- simple_calculator/js/simple_calculator.js
+++ simple_calculator/js/simple_calculator.js
@@ -1,3 +1,7 @@
 var dom_table = document.getElementById('cal');
 var dom_table_tbody = dom_table.children;
-var dom_table_tbody_tr = dom_table_tbody.children;
+var dom_table_tbody_tr = dom_table_tbody[0].children;
+console.log(dom_table_tbody.length);
+console.log(dom_table_tbody[0]);
+console.log(dom_table_tbody[1]);
+console.log(dom_table_tbody_tr);
