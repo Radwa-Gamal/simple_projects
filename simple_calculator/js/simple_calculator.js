2c7416326fc46b11c1ad90e75b48ba1b825012ae
Radwa <radwa.gamal92@gmail.com>
2014-12-14 23:21:05 +0400

    modify

--- simple_calculator/js/simple_calculator.js
+++ simple_calculator/js/simple_calculator.js
@@ -1,2 +1,3 @@
 var dom_table = document.getElementById('cal');
-var dom_table_tr = dom_table.children;
+var dom_table_tbody = dom_table.children;
+var dom_table_tbody_tr = dom_table_tbody.children;
