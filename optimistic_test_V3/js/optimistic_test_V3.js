fd1f47d0b1dab1c99e219d2e9cdb3fa3bbea0915
Radwa <radwa.gamal92@gmail.com>
2014-12-26 17:03:16 +0400

    modifying files

--- optimistic_test_V3/js/optimistic_test_V3.js
+++ optimistic_test_V3/js/optimistic_test_V3.js
@@ -5,21 +5,27 @@ var dom_div = document.getElementById('questions');
 var dom_all_divs = dom_div.children;
 var dom_inputs = dom_all_divs.children;
 var array_inputs = [];
+var i=1;
 
 for (var i=0; i < dom_all_divs.length; i++)
 {
-	for (var j=0; j < dom_all_divs[i].children.length; j++)
-	{
-		array_inputs.push(dom_all_divs[i].children[0]);
-	}
+	//for (var j=0; j < dom_all_divs[i].children.length; j++)
+	//{
+		array_inputs.push(dom_all_divs[i].children);
+	//}
 
 }
 
-console.log(array_inputs.length);
+
 
 var fn_next = function()
 {
-
+	//while (i<=10)
+	//{
+		console.log(array_inputs);
+		//array_inputs[i].style.display="";
+		//i++;
+	//}
 };
 
 dom_next_button.onclick = fn_next;
