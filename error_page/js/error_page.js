3cb7631cbc0e88de27e797470556207f12084691
Radwa <radwa.gamal92@gmail.com>
2015-03-15 11:16:35 +0400

    fixing video

--- error_page/js/error_page.js
+++ error_page/js/error_page.js
@@ -1,4 +1,4 @@
 
- $(document).ready(function() {
-    $("body").tubular({videoId: 'fpViZkhpPHk'}); // where idOfYourVideo is the YouTube ID. 
+ $('document').ready(function() {
+    $("#wrapper").tubular({videoId: 'fpViZkhpPHk'}); // where idOfYourVideo is the YouTube ID. 
   });
